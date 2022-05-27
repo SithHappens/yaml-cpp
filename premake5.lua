37498a924b4e3fd7219f6a8d235e3c47ed94b1f1
Stephan Reith <stephan.reith@gmx.de>
2022-05-27 21:50:59 +0200

    updates premake

--- premake5.lua
+++ premake5.lua
@@ -1,31 +1,36 @@
-project "yaml-cpp"
-  kind "StaticLib"
-  language "C++"
-  cppdialect "C++17"
-  systemversion "latest"
+project "yamlcpp"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++20"
+	staticruntime "off"
   
-  targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-  objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
   
-  files
-  {
-    "src/**.h",
-    "src/**.cpp",
-    "include/**.h"
-  }
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+		"include/**.h"
+	}
   
-  includedirs
-  {
-    "include"
-  }
-  
-  filter "system:windows"
-    staticruntime "on"
+	includedirs
+	{
+		"include"
+	}
 
-  filter "configurations:Debug"
-    runtime "Debug"
-    symbols "on"
+	defines
+	{
+		"YAML_CPP_STATIC_DEFINE"
+	}
   
-  filter "configurations:Release"
-    runtime "Release"
-    optimize "on"
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+	runtime "Debug"
+	symbols "on"
+  
+	filter "configurations:Release"
+	runtime "Release"
+	optimize "on"
